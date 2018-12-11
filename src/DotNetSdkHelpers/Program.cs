a34e2cc441ac33e3943c89f7cd6149b5c43b63cd
Jon Grythe Stødle <jonstodle@outlook.com>
2018-12-11 14:15:57 +0100

    Add project

---
+++ src/DotNetSdkHelpers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetSdkHelpers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
