9b8c0e9362e79de8f9bd61630a0613a9e376fbd4
Jon Grythe Stødle <jonstodle@outlook.com>
2018-12-11 16:30:15 +0100

    Move logic to get available releases to Program.cs

--- src/DotNetSdkHelpers/Commands/Releases.cs
+++ src/DotNetSdkHelpers/Commands/Releases.cs
@@ -11,17 +11,13 @@ namespace DotNetSdkHelpers.Commands
     [Command(Description = "Lists all available releases of .NET Core SDKs")]
     public class Releases
     {
-        public static string ReleasesJsonUrl =
-            "https://raw.githubusercontent.com/dotnet/core/master/release-notes/releases.json";
-
         [Option(Description = "Only show LTS releases")]
         public bool OnlyLts { get; }
 
         public async Task<int> OnExecuteAsync()
         {
-            var client = new HttpClient();
-            var releases = JsonConvert.DeserializeObject<List<Release>>(await client.GetStringAsync(ReleasesJsonUrl));
-
+            var releases = await Program.GetReleases();
+            
             foreach (var release in releases
                 .Where(r => !OnlyLts || r.IsRuntimeLts)
                 .OrderByDescending(r => r.SdkVersion))
--- src/DotNetSdkHelpers/Program.cs
+++ src/DotNetSdkHelpers/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using DotNetSdkHelpers.Commands;
 using McMaster.Extensions.CommandLineUtils;
+using Newtonsoft.Json;
 
 namespace DotNetSdkHelpers
 {
@@ -10,6 +13,15 @@ namespace DotNetSdkHelpers
     class Program
     {
         static void Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+
+        public static async Task<List<Release>> GetReleases()
+        {
+            var client = new HttpClient();
+            return JsonConvert.DeserializeObject<List<Release>>(
+                await client
+                    .GetStringAsync(
+                        "https://raw.githubusercontent.com/dotnet/core/master/release-notes/releases.json"));
+        }
 
         public Task<int> OnExecuteAsync(CommandLineApplication app)
         {
