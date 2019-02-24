13a7a1f82464bf64482cf1ab4428f10ca2a3e013
Jon Grythe Stødle <jonstodle@outlook.com>
2019-02-24 22:12:05 +0100

    Start work on list sub command

--- src/DotNetSdkHelpers.Fs/Program.fs
+++ src/DotNetSdkHelpers.Fs/Program.fs
@@ -4,8 +4,17 @@ open System
 open System.Diagnostics
 open Argu
 
-[<EntryPoint>]
-let main argv =
+type SDKHelperArguments =
+    | [<CliPrefix(CliPrefix.None)>] List
+    with
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | List -> "Lists all installed .NET Core SDKs"
+                
+
+
+let printSDKVersion =
     ProcessStartInfo("dotnet", "--version")
         |> (fun startInfo ->
             startInfo.RedirectStandardOutput <- true
@@ -14,4 +23,14 @@ let main argv =
         |> (fun output -> output.Trim())
         |> printfn "%s"
     0
-
+    
+[<EntryPoint>]
+let main argv =
+    let parser = ArgumentParser.Create<SDKHelperArguments>(programName = "dotnet-sdk")
+    
+    let result = parser.ParseCommandLine argv
+    
+    result.GetResult()
+    
+    printfn "Parse result: %A" <| result.GetAllResults()
+    0
