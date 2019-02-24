2b3ca1dfbe5bdf8348db4fc4479a8fd7f78a5c02
Jon Grythe Stødle <jonstodle@outlook.com>
2019-02-24 21:38:52 +0100

    Implement default response with no arguments or options

---
+++ src/DotNetSdkHelpers.Fs/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Diagnostics
+open Argu
+
+[<EntryPoint>]
+let main argv =
+    ProcessStartInfo("dotnet", "--version")
+        |> (fun startInfo ->
+            startInfo.RedirectStandardOutput <- true
+            Process.Start(startInfo))
+        |> (fun proc -> proc.StandardOutput.ReadToEnd())
+        |> (fun output -> output.Trim())
+        |> printfn "%s"
+    0 // return an integer exit code
+
