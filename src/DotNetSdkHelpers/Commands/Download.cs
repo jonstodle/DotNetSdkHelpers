cf9b4c802a3c3db7bb5f696dc0ec4085a1c5a81a
Jon Grythe Stødle <jonstodle@outlook.com>
2019-07-17 13:13:26 +0200

    Make download use the new formats and add support for more version names (lts, current, preview)

--- src/DotNetSdkHelpers/Commands/Download.cs
+++ src/DotNetSdkHelpers/Commands/Download.cs
@@ -16,48 +16,47 @@ namespace DotNetSdkHelpers.Commands
     public class Download : Command
     {
         // ReSharper disable UnassignedGetOnlyAutoProperty
-        [Argument(0, Description = "Version to download, or 'latest' (or empty)")]
-        public string Version { get; }
+        [Argument(0, Description = "'lts', 'current', 'preview' or a specific version. (Default: 'current')")]
+        public string Version { get; } = "current";
 
         [Option(CommandOptionType.SingleValue, Description =
             "The platform to download for. Defaults to the current platform on Windows and MacOS")]
         public string Platform { get; }
-
-        [Option(CommandOptionType.NoValue, Description = "Whether to include preview versions.")]
-        public bool IncludePreview { get; }
         // ReSharper restore UnassignedGetOnlyAutoProperty
 
         public override async Task Run()
         {
             Console.WriteLine("Resolving version to download...");
             
-            var version = Version ?? "latest";
-            var platform = GetPlatformString();
-
+            var platform = GetPlatformString() ?? Platform;
             if (platform is null)
                 throw new CliException("Unable to detect platform. Specify a platform using the --platform flag.");
 
-//            if (!((await GetReleaseChannels())
-//                .Where(r => IncludePreview || !r.IsPreview)
-//                .OrderByDescending(r => r.SdkVersion)
-//                .FirstOrDefault(r => version.Equals("latest") ||
-//                                     r.SdkVersion.StartsWith(version, StringComparison.OrdinalIgnoreCase))
-//                is ReleaseChannel release))
-//                throw new CliException("Unable to identify a release with specified version.");
-            var release = (await GetReleaseChannels()).First();
+            var release = await GetRelease(Version);
+            if (release is null)
+                throw new CliException($"Unable to resolve a version matching {Version}");
+            
+            // TODO: Check if version is already installed
 
-//            var downloadUrl = release[$"sdk-{platform}{GetPackageExtension(platform)}"];
-            var downloadUrl = release.Product;
-            var fileDownloadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                "Downloads", downloadUrl.Split('/').Last());
+            var file = release.Sdk.Files
+                .FirstOrDefault(f => f.Rid.Equals(platform, StringComparison.OrdinalIgnoreCase));
+            if (file?.Url is null)
+                throw new CliException($"Unable to find a download for platform '{platform}'");
+
+            var fileDownloadPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                "Downloads",
+                // ReSharper disable once PossibleNullReferenceException
+                file.Name);
+            
             var client = new HttpClient();
 
-            var downloadMessage = $"Downloading .NET Core SDK version {release.ChannelVersion} for {platform}";
+            var downloadMessage = $"Downloading .NET Core SDK version {release.Sdk.Version} for {platform}";
             
             using (var fileStream = new FileStream(fileDownloadPath, FileMode.Create))
-            using (var stream = await client.GetStreamAsync(downloadUrl))
+            using (var stream = await client.GetStreamAsync(file.Url))
             {
-                var buffer = new byte[(long) Math.Pow(2, 20)];
+                var buffer = new byte[(long)(20 * Math.Pow(2, 10))];
                 int bytesRead;
                 var bytesWritten = 0;
                 while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
@@ -71,6 +70,8 @@ namespace DotNetSdkHelpers.Commands
 
                 Console.WriteLine();
             }
+            
+            // TODO: Validate hash
 
             Process.Start(new ProcessStartInfo
             {
@@ -85,17 +86,48 @@ namespace DotNetSdkHelpers.Commands
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 return $"win-{architecture}";
             if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                return $"mac-{architecture}";
+                return $"osx-{architecture}";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return $"linux-{architecture}";
             return null;
         }
 
-        private string GetPackageExtension(string platform)
+        private async Task<Release> GetRelease(string version)
         {
-            if (platform.Contains("win", StringComparison.OrdinalIgnoreCase))
-                return ".exe";
-            if (platform.Contains("mac", StringComparison.OrdinalIgnoreCase))
-                return ".pkg";
-            return "";
+            var isPreview = version.Equals("preview", StringComparison.OrdinalIgnoreCase);
+            var isCurrent = version.Equals("current", StringComparison.OrdinalIgnoreCase);
+            var isLts = version.Equals("lts", StringComparison.OrdinalIgnoreCase);
+            
+            var channel = await GetReleaseChannel();
+            if (channel is null)
+                return null;
+
+            return await FindRelease();
+
+
+            async Task<ReleaseChannel> GetReleaseChannel()
+            {
+                var channels = await GetReleaseChannels();
+                if (isPreview)
+                    return channels.Find(c => c.SupportPhase == SupportPhases.Preview);
+                if (isCurrent)
+                    return channels.Find(c => c.SupportPhase == SupportPhases.Current);
+                if (isLts)
+                    return channels.Find(c => c.SupportPhase == SupportPhases.Lts);
+                
+                var vPrefix = string.Join("", version.Take(3));
+                return channels.Find(c => c.ChannelVersion.StartsWith(vPrefix, StringComparison.OrdinalIgnoreCase));
+            }
+
+            async Task<Release> FindRelease()
+            {
+                var releases = await GetReleases(channel.ReleasesJson);
+                if (isPreview || isCurrent || isLts)
+                    return releases.FirstOrDefault();
+                
+                return releases
+                    .FirstOrDefault(r => r.Sdk.Version.StartsWith(version, StringComparison.OrdinalIgnoreCase));
+            }
         }
     }
 }
